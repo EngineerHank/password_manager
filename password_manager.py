665e76ea155fa6ed77d6cbf163e0bc32eab1c218
hank <113780705+EngineerHank@users.noreply.github.com>
2023-11-05 22:01:02 +0300

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -16,7 +16,7 @@ def add():
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (ADD / VIEW / QUIT): ").lower()
-    if mode == "quit":
+    if mode == "quit": 
         break
     if mode == "add": 
         add()
